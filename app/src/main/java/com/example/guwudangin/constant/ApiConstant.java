02f815a5e93af2b8cf68dd4c0b56a250ea20f511
Alfian Haidar <alfianhaidar@gmail.com>
2020-11-20 20:08:47 +0700

    Login

---
+++ app/src/main/java/com/example/guwudangin/constant/ApiConstant.java
@@ -0,0 +1,6 @@
+package com.example.guwudangin.constant;
+
+public class ApiConstant {
+    public static final String IP = "192.168.0.110";
+    public static final String BASE_URL = "http://" + IP + ":8000/api/";
+}
